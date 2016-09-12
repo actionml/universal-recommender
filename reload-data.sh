88f501e1d155b7a00861691d6ad94253dc90ce04
Pavlenov Semen <me@pavlenov.ru>
2016-09-13 00:01:31 +0300

    remove develop file

--- reload-data.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-pio app data-delete default-rank -f
-python examples/import_default_rank.py --access_key '123456789' --file './data/sample-rank-data.txt'
-#python examples/import_handmade.py --access_key '123456789' --file './data/sample-handmade-data.txt'
-
-pio build
-pio train -- --executor-memory 1g --driver-memory 1g --master local
